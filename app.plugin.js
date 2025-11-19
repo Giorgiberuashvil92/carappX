141c85030e1959534af995371598f37d9a3aebc8
Giorgiberuashvil92 <gberuashvili92@gmail.com>
2025-11-19 23:23:42 +0400

    Update plugin to also preserve Info.plist CFBundleIconFiles

--- app.plugin.js
+++ app.plugin.js
@@ -1,4 +1,4 @@
-const { withXcodeProject, IOSConfig } = require('@expo/config-plugins');
+const { withXcodeProject, withInfoPlist } = require('@expo/config-plugins');
 const path = require('path');
 
 /**
@@ -6,7 +6,60 @@ const path = require('path');
  * This ensures icons are included in the bundle for iOS 10 compatibility
  */
 const withLegacyIcons = (config) => {
-  return withXcodeProject(config, (config) => {
+  // First, ensure Info.plist has CFBundleIconFiles
+  config = withInfoPlist(config, (config) => {
+    const plist = config.modResults;
+    
+    // Ensure CFBundleIconName exists
+    if (!plist.CFBundleIconName) {
+      plist.CFBundleIconName = 'AppIcon';
+    }
+    
+    // Ensure CFBundleIcons exists with legacy icon files
+    if (!plist.CFBundleIcons) {
+      plist.CFBundleIcons = {};
+    }
+    if (!plist.CFBundleIcons.CFBundlePrimaryIcon) {
+      plist.CFBundleIcons.CFBundlePrimaryIcon = {};
+    }
+    if (!plist.CFBundleIcons.CFBundlePrimaryIcon.CFBundleIconFiles) {
+      plist.CFBundleIcons.CFBundlePrimaryIcon.CFBundleIconFiles = [];
+    }
+    
+    // Add legacy icon files if not present
+    const iconFiles = plist.CFBundleIcons.CFBundlePrimaryIcon.CFBundleIconFiles;
+    if (!iconFiles.includes('Icon-60@2x')) {
+      iconFiles.push('Icon-60@2x');
+    }
+    if (!iconFiles.includes('Icon-60@3x')) {
+      iconFiles.push('Icon-60@3x');
+    }
+    
+    // Ensure CFBundleIcons~ipad exists
+    if (!plist['CFBundleIcons~ipad']) {
+      plist['CFBundleIcons~ipad'] = {};
+    }
+    if (!plist['CFBundleIcons~ipad'].CFBundlePrimaryIcon) {
+      plist['CFBundleIcons~ipad'].CFBundlePrimaryIcon = {};
+    }
+    if (!plist['CFBundleIcons~ipad'].CFBundlePrimaryIcon.CFBundleIconFiles) {
+      plist['CFBundleIcons~ipad'].CFBundlePrimaryIcon.CFBundleIconFiles = [];
+    }
+    
+    // Add iPad icon files if not present
+    const ipadIconFiles = plist['CFBundleIcons~ipad'].CFBundlePrimaryIcon.CFBundleIconFiles;
+    if (!ipadIconFiles.includes('Icon-76@2x')) {
+      ipadIconFiles.push('Icon-76@2x');
+    }
+    if (!ipadIconFiles.includes('Icon-83.5@2x')) {
+      ipadIconFiles.push('Icon-83.5@2x');
+    }
+    
+    return config;
+  });
+  
+  // Then, add icons to Xcode project
+  config = withXcodeProject(config, (config) => {
     const project = config.modResults;
     
     // Icon files to add
@@ -43,6 +96,8 @@ const withLegacyIcons = (config) => {
 
     return config;
   });
+  
+  return config;
 };
 
 module.exports = (config) => {
